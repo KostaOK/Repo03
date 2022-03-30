03afb4190db8001460fc51fa0089461ab84b5265
Niko Kostič <kostic.niko@icloud.com>
2022-03-30 19:43:30 +0200

    2. naloga commenting

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main () {
+    // Only change code below this line
+    // To vadim za pisanje komentarjev za takrat, ko bom sprejet v naslednji krog
+    /*In enkrat ko bom pisal dolge kode bom
+    moral še več stvari pokomentirat
+    */
+    // Only change code above this line
+
+}
+main ();
+
+module.exports = main;
