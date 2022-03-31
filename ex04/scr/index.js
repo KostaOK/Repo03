b351e4f41d0c6a85aae2bffc794cae0af5e57d18
Niko Kostič <kostic.niko@icloud.com>
2022-03-31 09:02:43 +0200

    4. naloga zdaj pa znam osnove js

--- ex04/scr/index.js
+++ ex04/scr/index.js
@@ -1,12 +1,13 @@
-function main (x,y) {
+function main () {
     // Only change code below this line
     var x = 10;
     var y = x;
     // Only change code above this line
+    x = 10;
+    y = x;
     return [x,y];
 }
 
-console.log(main(x));
-console.log(main(y));
+console.log(main());
 
 module.exports = main;
