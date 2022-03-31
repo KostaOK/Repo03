8427c654150cf11787193cc997586fdd1f715267
Niko Kostič <kostic.niko@icloud.com>
2022-03-31 08:47:12 +0200

    pravilna 9ta

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -9,7 +9,7 @@ function main (numOne, numTwo) {
 }
 
 console.log(main(15, 8)); // change this line
-console.log(main(25, 28)); // change this line
+console.log(main(25, 8)); // change this line
 console.log(main(425, 120)); // change this line
 
 module.exports = main;
