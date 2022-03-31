6880209ca3cfe21fbc2a5d3935b9ac458a58cac5
Niko Kostič <kostic.niko@icloud.com>
2022-03-31 09:31:01 +0200

    7. naloga

---
+++ ex07/src/index.js
@@ -0,0 +1,21 @@
+function main() {
+    // Only change code below this line
+    // Variable declarations
+    var StUdLyCapVar;
+    var properCamelCase;
+    var TitleCaseOver;
+    
+    // Variable assignments
+    STUDLYCAPVAR = 10;
+    PRoperCAmelCAse = "A String";
+    tITLEcASEoVER = 9000;
+    // Only change code above this line
+    return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+    }
+
+    console.log(main());
+    module.exports = main;
+}
